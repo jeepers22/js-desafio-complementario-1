902db03779e7d8a403dccafc5cf2bed308365f80
Maximiliano Safran <maxizero@gmail.com>
2022-08-24 23:39:39 -0300

    sumamos condicional para que sólo admita ingresos numéricos y positivos

--- index.js
+++ index.js
@@ -3,10 +3,15 @@ let valorEntero = 0
 
 while (valor != "esc" && valor != "ESC") {
     valorEntero = parseInt(valor)
-    alert(`Se mostrará en consola la tabla del número ${valor}`)
-    console.log(`\nLa tabla del ${valor} es la siguiente:`)
-    for (let i = 0; i <= 10; i++) {
-        console.log(`${valor} x ${i} = ${valorEntero * i}`)
+    if (valorEntero > 0) {
+        alert(`Se mostrará en consola la tabla del número ${valor}`)
+        console.log(`\nLa tabla del ${valor} es la siguiente:`)
+        for (let i = 0; i <= 10; i++) {
+            console.log(`${valor} x ${i} = ${valorEntero * i}`)
+        }
+    }
+    else {
+        alert("Debe ingresar un número positivo")
     }
     valor = prompt("Ingrese próximo número del que desea obtener su tabla:  (esc/ESC para finalizar ejecución)")
 }
