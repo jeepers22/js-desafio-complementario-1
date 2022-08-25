8355401fb1bc091f91c57e381cbb3d7e7e2cce99
Maximiliano Safran <maxizero@gmail.com>
2022-08-24 23:22:04 -0300

    commit con ejercicio terminado

---
+++ README.md
@@ -0,0 +1,3 @@
+Primer desafío complementario
+
+Realizar un algoritmo utilizando cíclos para ejecutar repetición de un bloque de código
---
+++ index.js
@@ -0,0 +1,14 @@
+let valor = prompt("Ingrese número del que desea obtener su tabla: (esc/ESC para finalizar ejecución)")
+let valorEntero = 0
+
+while (valor != "esc" && valor != "ESC") {
+    valorEntero = parseInt(valor)
+    alert("Se mostrará en consola la tabla del número ingresado")
+    console.log(`\nLa tabla del ${valor} es la siguiente:`)
+    for (let i = 0; i <= 10; i++) {
+        console.log(`${valor} x ${i} = ${valorEntero * i}`)
+    }
+    valor = prompt("Ingrese próximo número del que desea obtener su tabla:  (esc/ESC para finalizar ejecución)")
+}
+
+alert("Ha finalizado la ejecución")
